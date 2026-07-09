8fca5636932a23a2f6fe447e749403f25a923812
Aasim-Qureshi <aasimq194@gmail.com>
2026-07-09 10:58:57 +0530

    fixing bugs

--- dist/machine-valuation/inspector-files.constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MV_INSPECTOR_FILE_MAX_BYTES = void 0;
-exports.MV_INSPECTOR_FILE_MAX_BYTES = 100 * 1024 * 1024;
--- dist/server/models/mobasherAuctions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MOBASHER_AUCTIONS_COLLECTION = void 0;
-exports.getMobasherAuctionsCollection = getMobasherAuctionsCollection;
-exports.MOBASHER_AUCTIONS_COLLECTION = "mobasherAuctions";
-function getMobasherAuctionsCollection(db) {
-    return db.collection(exports.MOBASHER_AUCTIONS_COLLECTION);
-}
